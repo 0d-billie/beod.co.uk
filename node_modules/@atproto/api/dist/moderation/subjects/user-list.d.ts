558da871328c8397647569bbc12ce0c9711b343e
bod <billie.odwyer@tiger-computing.co.uk>
2024-11-29 14:25:34 +0000

    Add bluesky comments to blog

---
+++ node_modules/@atproto/api/dist/moderation/subjects/user-list.d.ts
@@ -0,0 +1,4 @@
+import { ModerationDecision } from '../decision';
+import { ModerationSubjectUserList, ModerationOpts } from '../types';
+export declare function decideUserList(subject: ModerationSubjectUserList, opts: ModerationOpts): ModerationDecision;
+//# sourceMappingURL=user-list.d.ts.map
